5b1e97b510190515f8f3c5a65a198076295935ba
Kian Torab <kianabc@kianabc.com>
2017-09-13 13:38:34 -0600

    ***** NPMK Version 4.5.2.0 :September 13, 2017 *****
    % NPMKverChecker 1.0.1.0: September 13, 2017
    %   - Fixed a crash in case there is no Internet connection.
    %

--- NPMK/NPMKverChecker.m
+++ NPMK/NPMKverChecker.m
@@ -18,15 +18,21 @@ function NPMKverChecker()
 % 1.0.0.0: September 13, 2017
 %   - Initial release.
 %
+% 1.0.1.0: September 13, 2017
+%   - Fixed a crash in case there is no Internet connection.
+%
 
 %% Check for the latest version fo NPMK
-FIDv = fopen('Versions.txt');
-verFile = fscanf(FIDv, '%s'); 
-fclose(FIDv);
-latestVersion = verFile(findstr('LATEST', verFile)+7:findstr('LATEST', verFile)+13);
-gitHubPage = urlread('https://github.com/BlackrockMicrosystems/NPMK/releases/latest');
-newVersionAvailable = findstr(latestVersion, gitHubPage);
-if isempty(newVersionAvailable)
-    disp('A new version of NPMK may be available.');
-    disp('Please visit https://github.com/BlackrockMicrosystems/NPMK/releases/latest to get the latest version.');
+try
+    FIDv = fopen('Versions.txt');
+    verFile = fscanf(FIDv, '%s'); 
+    fclose(FIDv);
+    latestVersion = verFile(findstr('LATEST', verFile)+7:findstr('LATEST', verFile)+13);
+    gitHubPage = urlread('https://github.com/BlackrockMicrosystems/NPMK/releases/latest');
+    newVersionAvailable = findstr(latestVersion, gitHubPage);
+    if isempty(newVersionAvailable)
+        disp('A new version of NPMK may be available.');
+        disp('Please visit https://github.com/BlackrockMicrosystems/NPMK/releases/latest to get the latest version.');
+    end
+catch
 end
