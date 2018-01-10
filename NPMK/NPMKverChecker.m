9fdf3c0ceec14e2e4e84b82331490a949057366a
Kian Torab <kianabc@kianabc.com>
2018-01-10 10:35:06 -0700

    % NPMKverChecker 1.0.2.0: January 10, 2018 %   - Added a clickable URL to the prompt.

--- NPMK/NPMKverChecker.m
+++ NPMK/NPMKverChecker.m
@@ -21,6 +21,11 @@ function NPMKverChecker()
 % 1.0.1.0: September 13, 2017
 %   - Fixed a crash in case there is no Internet connection.
 %
+% 1.0.2.0: January 10, 2018
+%   - Added a clickable URL to the prompt.
+%
+
+gitHubURL = 'https://github.com/BlackrockMicrosystems/NPMK/releases/latest';
 
 %% Check for the latest version fo NPMK
 try
@@ -28,11 +33,11 @@ try
     verFile = fscanf(FIDv, '%s'); 
     fclose(FIDv);
     latestVersion = verFile(findstr('LATEST', verFile)+7:findstr('LATEST', verFile)+13);
-    gitHubPage = urlread('https://github.com/BlackrockMicrosystems/NPMK/releases/latest');
+    gitHubPage = urlread(gitHubURL);
     newVersionAvailable = findstr(latestVersion, gitHubPage);
     if isempty(newVersionAvailable)
         disp('A new version of NPMK may be available.');
-        disp('Please visit https://github.com/BlackrockMicrosystems/NPMK/releases/latest to get the latest version.');
+        fprintf('Please visit <a href="%s">GitHub NPMK Page</a> to get the latest version.\n', gitHubURL)
     end
 catch
 end
