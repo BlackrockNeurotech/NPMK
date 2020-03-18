993a3774cdcf3eac4de342d9c60e90b050d5d646
kianabc <kianabc@kians-mbp.lan>
2020-03-18 08:15:03 -0600

    Added support for unix file structure during file opening.

--- NPMK/Dependent Functions/getSettingFileFullPath.m
+++ NPMK/Dependent Functions/getSettingFileFullPath.m
@@ -1,10 +1,24 @@
 function settingFileFullPath = getSettingFileFullPath(functionName)
+% settingFileFullPath
+% 
+% Returns the full path of the settings file.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Version History
+%
+% 1.0.0.0:
+%   - Initial release.
+%
+% 1.1.0.0:
+%   - Updated to support unix file system.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 settingFileFullPath = which(functionName);
 
 if ~isempty(settingFileFullPath)
     settingFilePath = fileparts(settingFileFullPath);
-    if ismac
+    if ismac || isunix
         settingFileFullPath = [settingFilePath '/' functionName '.ini'];
     else
         settingFileFullPath = [settingFilePath '\' functionName '.ini'];
